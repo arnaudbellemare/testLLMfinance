9fa462e8f45ae264d95680d457bc9fc0b0030b4a
gucci <arnaudbellemare@hotmail.com>
2025-10-28 17:28:57 -0400

    Update llmfinance.py

--- llmfinance.py
+++ llmfinance.py
@@ -7,6 +7,7 @@ import numpy as np
 import yfinance as yf
 from datetime import datetime, timedelta
 import math
+import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm import tqdm
 import logging
